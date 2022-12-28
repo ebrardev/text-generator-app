bb3918d16d711d4444f419190b5238576aa881c2
Ebrar <16155052@mersin.edu.tr>
2022-12-28 22:35:37 +0300

    textcontainer added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Header from './components/Header/header';
 import Input from './components/Input/Input';
+import TextContent from './components/TextContent/textcontent';
 
 function App() {
   return (
     <div className="App">
       <Header />
      <Input/>
+     < TextContent />
     </div>
   );
 }
---
+++ src/components/TextContent/textcontent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './style.css'
+function textcontent() {
+  return (
+    <div className='text-content'>textcontent</div>
+  )
+}
+
+export default textcontent
